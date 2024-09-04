0cd9e5ddf0354378f4172300450c0e6329bfb2f1
IITApurba <156194221+IITApurba@users.noreply.github.com>
2024-09-04 23:30:16 +0530

    Create Lower_bound.cpp

---
+++ Module 1 Week 3/Binary Search/Live Session/Lower_bound.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortened types
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+// Constants
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll LLINF = 1e18;
+
+// Utility functions
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define per(i, a, b) for (int i = (b); i-- > (a);)
+
+// Input and Output
+#define fast_io()                \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+// Debugging
+#define debug(x) cerr << #x << " = " << (x) << endl;
+
+// Function to read multiple inputs
+template <typename T>
+void read(T &t) { cin >> t; }
+
+template <typename T, typename... Args>
+void read(T &t, Args &...args)
+{
+    cin >> t;
+    read(args...);
+}
+
+// Function to print multiple outputs
+template <typename T>
+void print(T t) { cout << t << '\n'; }
+
+template <typename T, typename... Args>
+void print(T t, Args... args)
+{
+    cout << t << ' ';
+    print(args...);
+}
+
+int n;
+vi v(100100,0);
+
+int A = 4;
+
+int check(int x)
+{
+    return v[x] >= A;
+}
+
+void solve()
+{
+
+    read(n);
+    for (int i = 0; i < n; i++)
+        read(v[i]);
+
+    int lo = 0;
+    int hi = n - 1;
+    int ans = 0;
+
+    while (lo <= hi)
+    {
+        int mid = (lo + hi) / 2;
+        if (check(mid))
+        {
+            ans = mid;
+            hi = mid - 1;
+        }
+        else
+        {
+            lo = mid + 1;
+        }
+    }
+
+    // Using STL lower_bound function for comparison
+    int low1 = lower_bound(v.begin(), v.begin() + n, A) - v.begin();
+
+    cout << "Index of lower bound of A: " << ans << endl;
+    cout << "Index of lower bound of 30: " << low1 << endl;
+
+    if (ans < n)
+        cout << "Element at lower bound of A: " << v[ans] << endl;
+    else
+        cout << "No element >= A found" << endl;
+
+    if (low1 < n)
+        cout << "Element at lower bound of 30: " << v[low1] << endl;
+    else{
+        cout << "No element >= 30 found" << endl;
+    }
+}
+// Main function
+int main()
+{
+    fast_io();
+
+    int t = 1;
+    // read(t);
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
