d3eb02189c01c154ae2cc74a49afb5fe6f52ae6a
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2024-09-14 00:25:49 +0530

    Add files via upload

---
+++ Module 1 Week 3/Binary Search/Practice Problems/Kth_Sum_Value.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortened types
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+// Constants
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll LLINF = 1e18;
+
+// Utility functions
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define per(i, a, b) for (int i = (b); i-- > (a); )
+
+// Input and Output
+#define fast_io() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+// Debugging
+#define debug(x) cerr << #x << " = " << (x) << endl;
+
+// Function to read multiple inputs
+template<typename T>
+void read(T& t) { cin >> t; }
+
+template<typename T, typename... Args>
+void read(T& t, Args&... args) { cin >> t; read(args...); }
+
+// Function to print multiple outputs
+template<typename T>
+void print(T t) { cout << t << '\n'; }
+
+template<typename T, typename... Args>
+void print(T t, Args... args) { cout << t << ' '; print(args...); }
+
+int n;
+int m;
+int k;
+int a[100100];
+int b[100100];
+
+void solve(){
+
+    read(n, m, k);
+
+    //read a
+    rep(i, 0, n) read(a[i]);
+    
+    //read b
+    rep(i, 0, m) read(b[i]);
+
+    // take a as smaller array
+    if (n > m)
+    {
+        swap(a, b);
+        swap(n, m);
+    }
+
+    //sort
+    sort(a, a + n);
+    sort(b, b + m);
+
+    //binary search
+    int lo = a[0] + b[0];
+    int hi = a[n - 1] + b[m - 1];
+
+    int ans = lo;
+
+    while (lo<=hi)
+    {
+        int mid = lo + (hi - lo) / 2;
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int x = upper_bound(b, b + m, mid - a[i]) - b;
+            cnt += x;
+        }
+
+        if (cnt >= k)
+        {
+            ans = mid;
+            hi = mid - 1;
+        }
+        else{
+            lo = mid + 1;
+        }
+        
+    }
+    print(ans);
+    
+
+}
+// Main function
+int main() {
+    fast_io();
+    
+    int t = 1;
+    // read(t);
+    while(t--) {
+        solve();
+    }
+    
+    
+    
+    return 0;
+}
